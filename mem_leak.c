d9c959b58aad37617b6c7ad97b5c398c54914a26
hayama17 <hanetarou01@gmail.com>
2022-09-07 04:42:27 +0000

    first commit

---
+++ mem_leak.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MB 1024*1024
+
+int main(int argc, char *argv[])
+{
+    char *p;
+    int malloc_size = 10 * MB;
+    int i=0;
+
+    while(1)
+    {
+        ++i;
+        printf("test:%d\n",i);
+        p = (char *)malloc(malloc_size);
+        memset(p, 0, malloc_size);
+        sleep(1);
+        
+    }
+
+
+    return 0;
+}
