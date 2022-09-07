d5f1fd8ecc3262fb7f62ecf059061ea7a7e8e9cf
hayama <64768057+hayama17@users.noreply.github.com>
2022-09-07 15:06:36 +0900

    Update mem_leak.c

--- mem_leak.c
+++ mem_leak.c
@@ -8,13 +8,13 @@
 int main(int argc, char *argv[])
 {
     char *p;
-    int malloc_size = 100 * MB;
+    int malloc_size = 10 * MB;
     int i=0;
 
     while(1)
     {
         ++i;
-        printf("mem:%dMB\n",i*100);
+        printf("mem:%dMB\n",i*10);
         p = (char *)malloc(malloc_size);
         memset(p, 0, malloc_size);
         sleep(1);
